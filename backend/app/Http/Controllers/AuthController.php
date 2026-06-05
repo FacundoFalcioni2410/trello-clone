abb702921cd6ac7c6b8cbdd18fc8e03f21b77b96
FacundoFalcioni2410 <facundofalcioni2410@gmail.com>
2026-06-05 13:27:25 -0300

    chore: project init, auth setup

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller {
+
+    public function register(Request $request) {
+        $request->validate([
+            "name"=> ['required', 'string'],
+            "email"=> ['required', 'email'],
+            'password'=> ['required'],
+        ]);
+
+        $user = User::create([
+            'name'=> $request->name,
+            'email'=> $request->email,
+            'password'=> Hash::make($request->password),
+        ]);
+                        
+
+        return response()->json([
+            'status'=> 'success',
+            'message'=> 'User registered'
+        ]);
+    }
+
+    public function login(Request $request) {
+        $credentials = $request->validate([
+            "email"=> ['required', 'email'],
+            'password'=> ['required'],
+        ]);
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json([
+                'error'=> 'Wrong credentials',
+            ],401);
+        }
+
+        $request->session()->regenerate();
+
+        return response()->json([
+            'user' => Auth::user()
+        ]);
+    }
+}
+?>
